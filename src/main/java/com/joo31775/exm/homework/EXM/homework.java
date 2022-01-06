d8f1505081d2ef35cb5d25616154b632e0811ade
Big_Plate <great9744@gmail.com>
2022-01-06 21:15:49 +0900

    Initial commit

---
+++ src/main/java/com/joo31775/exm/homework/EXM/homework.java
@@ -0,0 +1,17 @@
+package com.joo31775.exm.homework.EXM;
+
+import org.bukkit.plugin.java.JavaPlugin;
+public final class homework extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().warning("Hello, World!");
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
